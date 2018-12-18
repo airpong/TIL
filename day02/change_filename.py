1757ab954b26dedfa015250bbed3197a622d0fbb
Changun Ha <giponge@gmail.com>
2018-12-18 12:08:26 +0900

    chang_filename

---
+++ day02/change_filename.py
@@ -0,0 +1,6 @@
+import os
+
+os.chdir(r'C:\Users\student\hcu\day02\dummy')
+#print(os.getcwd())
+for filename in os.listdir(''):    #os 모듈에게 listdir(디렉토리의 리스트를 가지고 와라)/'.' 모든파일!
+    os.rename(filename,filename.replace('지원자','합격자'))    #rename(바꾸고자 하는 기존 이름, 바꿀 이름)
