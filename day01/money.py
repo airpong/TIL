5f5484553b4dde6679923f9c4c46adf1155d90a3
Changun Ha <giponge@gmail.com>
2018-12-18 09:38:05 +0900

    Second commit

--- day01/money.py
+++ day01/money.py
@@ -1,8 +1,10 @@
 import requests
 import bs4
+
 response = requests.get('https://finance.naver.com/marketindex/').text
-# ctrl + ``
 soup = bs4.BeautifulSoup(response, 'html.parser')
 result= soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
+
 print('미국 환율은 '+result+' 입니다')
-print("미국 환율은 {0} 입니다.".format(result))
+print("미국 환율은 {0} 입니다!!!".format(result))
+print('^o^')
