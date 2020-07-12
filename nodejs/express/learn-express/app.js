bf75cfb435b333ac81be4db0f3accb0423e8cfca
Changun Ha <giponge@gmail.com>
2020-07-13 03:25:23 +0900

    nodejs|express

--- nodejs/express/learn-express/app.js
+++ nodejs/express/learn-express/app.js
@@ -4,42 +4,25 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
-const flash = require("connect-flash");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
 var app = express();
+const flash = require("connect-flash");
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("secret code"));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "secret code",
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
-);
-app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
