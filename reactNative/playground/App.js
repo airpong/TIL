5208b8c4ffc858aa629a3160b2339c8efbb53a79
Changun Ha <giponge@gmail.com>
2020-08-06 19:43:17 +0900

    textinput change aniamtion

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,13 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Dimensions, TextInput } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import React, { useState, useRef } from "react";
+
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+
 export default function App() {
-  const [ab, setAb] = useState(() => {
-    return { hello: "abc" };
-  });
-  console.log(ab);
-  return <View style={styles.container}></View>;
+  const [userName, setUserName] = useState("하창언");
+  const change = (text) => {
+    setUserName(text);
+  };
+  const ttt = useRef(0);
+
+  const click = () => {};
+  return (
+    <>
+      <View style={styles.container}>
+        <View
+          ref={ttt}
+          style={{
+            backgroundColor: "blue",
+            height: 40,
+            width: 110,
+            zIndex: ttt,
+          }}
+        >
+          <Text>{userName}</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "red",
+            height: 40,
+            width: 100,
+            position: "absolute",
+          }}
+        >
+          <TextInput
+            placeholder={userName}
+            style={{
+              fontSize: 17,
+            }}
+            onChangeText={(Text) => change(Text)}
+          />
+        </View>
+      </View>
+      <View style={{ margin: 40 }}>
+        <Button title="click" style={{ marginTop: 50 }} onPress={click} />
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -15,11 +53,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 50,
-  },
-  textContainer: {
-    flexDirection: "row",
-
-    justifyContent: "center",
   },
 });
