dfff64ae8749f28b82a84b04f2b55a4fc63033df
Changun Ha <giponge@gmail.com>
2020-09-03 15:50:08 +0900

    til

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import * as React from "react";
+import { Button, Image, View, Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export default () => {
-  return <View></View>;
+  return <View style={{ flex: 1 }}></View>;
 };
