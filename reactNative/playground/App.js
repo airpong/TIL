f7ce1071e2dd26b7695fd3fd1b0c686cbdda1e06
Changun Ha <giponge@gmail.com>
2020-09-10 16:23:06 +0900

    expo secure token

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,22 +1,32 @@
 import * as React from "react";
 import { Button, Image, View, Platform } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import * as SecureStore from "expo-secure-store";
+const MY_SECURE_AUTH_STATE_KEY = "MySecureAuthStateKey";
+const saveToken = async () => {
+  const saveResult = await SecureStore.setItemAsync(
+    MY_SECURE_AUTH_STATE_KEY,
+    "secureKey"
+  );
 
+  console.log("saveResult: ", saveResult);
+};
+const loadToken = async () => {
+  const loadToken = await SecureStore.getItemAsync(MY_SECURE_AUTH_STATE_KEY);
+  console.log("loadToken: ", loadToken);
+};
 export default () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <MapView
-        style={{ width: "100%", height: "100%" }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: 127.4324,
-          latitudeDelta: 0.922,
-          longitudeDelta: 0.421,
-        }}
-      >
-        <Marker coordinate={{ latitude: 37.78825, longitude: 127.4324 }} />
-        <Marker coordinate={{ latitude: 37.78825, longitude: 127.3324 }} />
-      </MapView>
+      <Button
+        style={{ width: 100, height: 100 }}
+        title="토큰저장"
+        onPress={saveToken}
+      ></Button>
+      <Button
+        style={{ width: 100, height: 100 }}
+        title="토큰 가져오기"
+        onPress={loadToken}
+      ></Button>
     </View>
   );
 };
