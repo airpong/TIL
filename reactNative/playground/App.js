85b2d13f4de24135b82129878293f4793562a67b
Changun Ha <giponge@gmail.com>
2020-09-10 15:02:21 +0900

    til

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -4,5 +4,12 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 
 export default () => {
-  return <View style={{ flex: 1 }}></View>;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Image
+        source={require("./assets/100.png")}
+        style={{ width: 100, height: 100 }}
+      ></Image>
+    </View>
+  );
 };
