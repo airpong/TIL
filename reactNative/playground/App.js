c22bb55874fb0c6fdbd81089328f2fefd8a4d912
Changun Ha <giponge@gmail.com>
2020-09-10 16:17:38 +0900

    til

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,15 +1,22 @@
 import * as React from "react";
 import { Button, Image, View, Platform } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
+import MapView, { Marker } from "react-native-maps";
 
 export default () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Image
-        source={require("./assets/100.png")}
-        style={{ width: 100, height: 100 }}
-      ></Image>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: 127.4324,
+          latitudeDelta: 0.922,
+          longitudeDelta: 0.421,
+        }}
+      >
+        <Marker coordinate={{ latitude: 37.78825, longitude: 127.4324 }} />
+        <Marker coordinate={{ latitude: 37.78825, longitude: 127.3324 }} />
+      </MapView>
     </View>
   );
 };
