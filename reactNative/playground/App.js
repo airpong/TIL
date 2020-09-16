172edf694eb0eff27654061efaae6b5bd5e0994a
Changun Ha <giponge@gmail.com>
2020-09-16 17:03:39 +0900

    til

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,32 +1,37 @@
 import * as React from "react";
-import { Button, Image, View, Platform } from "react-native";
-import * as SecureStore from "expo-secure-store";
-const MY_SECURE_AUTH_STATE_KEY = "MySecureAuthStateKey";
-const saveToken = async () => {
-  const saveResult = await SecureStore.setItemAsync(
-    MY_SECURE_AUTH_STATE_KEY,
-    "secureKey"
-  );
+import {
+  Button,
+  Text,
+  Image,
+  View,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 
-  console.log("saveResult: ", saveResult);
-};
-const loadToken = async () => {
-  const loadToken = await SecureStore.getItemAsync(MY_SECURE_AUTH_STATE_KEY);
-  console.log("loadToken: ", loadToken);
-};
 export default () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button
-        style={{ width: 100, height: 100 }}
-        title="토큰저장"
-        onPress={saveToken}
-      ></Button>
-      <Button
-        style={{ width: 100, height: 100 }}
-        title="토큰 가져오기"
-        onPress={loadToken}
-      ></Button>
+    <View style={{ flex: 1 }} accessible={true}>
+      <View style={{ height: "50%", backgroundColor: "red" }}>
+        <TouchableOpacity
+          style={{
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>abc</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={{
+          height: "30%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>allbe</Text>
+      </TouchableOpacity>
+      <Text>abc</Text>
     </View>
   );
 };
