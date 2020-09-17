e3046f83e1eaca87532afac56724987e923b1c54
Changun Ha <giponge@gmail.com>
2020-09-17 16:36:47 +0900

    til

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,49 +1,47 @@
-import * as React from "react";
-import {
-  Button,
-  Text,
-  Image,
-  View,
-  Platform,
-  TouchableOpacity,
-} from "react-native";
-import HaversineGeolocation from "haversine-geolocation";
-const points = [
-  {
-    latitude: 37.5379387,
-    longitude: 127.066518,
-  },
-  {
-    latitude: 37.5447048,
-    longitude: 127.0663154,
-  },
-];
-const abc = HaversineGeolocation.getDistanceBetween(points[0], points[1], "m"); // 1133062.7 m
-console.log(Math.round(abc / 10) * 10);
-export default () => {
-  return (
-    <View style={{ flex: 1 }} accessible={true}>
-      <View style={{ height: "50%", backgroundColor: "red" }}>
-        <TouchableOpacity
-          style={{
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text>abc</Text>
-        </TouchableOpacity>
+import React, { Component } from "react";
+import { Button, Linking, View, StyleSheet } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import Constants from "expo-constants";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Open URL with ReactNative.Linking"
+          onPress={this._handleOpenWithLinking}
+          style={styles.button}
+        />
+        <Button
+          title="Open URL with Expo.WebBrowser"
+          onPress={this._handleOpenWithWebBrowser}
+          style={styles.button}
+        />
       </View>
-      <TouchableOpacity
-        style={{
-          height: "30%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>allbe</Text>
-      </TouchableOpacity>
-      <Text>abc</Text>
-    </View>
-  );
-};
+    );
+  }
+
+  _handleOpenWithLinking = async () => {
+    console.log(
+      await Linking.canOpenURL("market://details?id=net.daum.android.map")
+    );
+    Linking.openURL("market://details?id=net.daum.android.map");
+  };
+
+  _handleOpenWithWebBrowser = () => {
+    WebBrowser.openBrowserAsync("https://expo.io");
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+  },
+  button: {
+    marginVertical: 10,
+  },
+});
