1f77dfaf9a7a842e0cdf169e249807fa5747ed61
Changun Ha <giponge@gmail.com>
2020-09-18 18:03:14 +0900

    restart react native app

--- reactNative/playground/App.js
+++ reactNative/playground/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import { Button, View, StyleSheet } from "react-native";
-import renderer from "react-test-renderer";
-
+import { Restart } from "fiction-expo-restart";
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: "red" }}>
-        <View></View>
+      <View
+        style={{
+          flex: 1,
+
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          title="restart!"
+          onPress={() => {
+            Restart();
+          }}
+        />
       </View>
     );
   }
